89c69eacfda6657834a41d104ac3affa8619bf92
Sebastian Cristobal Mendoza Acuña <Sebastian Cristobal Mendoza Acua>
2016-10-25 11:19:52 -0300

    Se completa codigo de index

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo "Hola Mundo";?>
+<doctype />
+<html>
+	<head>
+	
+	</head>
+	
+	<body>
+		<h1>
+			<?php echo "Hola Mundo";?>		
+		</h1>
+		
+		<?php 
+		for($i=2; $i<6; $i++)
+		echo "<h$i>Como estas? </h$i>";
+		
+		?>
+	</body>
+
+</html>
+
